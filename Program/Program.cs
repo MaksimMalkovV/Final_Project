01d1ccabf51ae7d112df5506f44b49cdb33e932b
MaksimMalkovV <maer2971@gmail.com>
2022-11-25 12:59:09 +0300

    1.0 Version

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,3 +4,28 @@
 // При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+// ["1234", "1586", "-2"] -> ["-2"]
+string[] array1 = new string[5];
+// 5 - колличество элементов массива
+int len = 3;
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine()!);
+    if (element.Length <= len)
+        array1[i] = element;
+}
+
+Console.WriteLine();
+PrintArray(array1);
+
+void PrintArray(string[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]} ");
+    }
+    Console.WriteLine();
+}
