bc5506062e17a3e4d0b46f6cf1fc9ac83c854e37
MaksimMalkovV <maer2971@gmail.com>
2022-11-27 17:07:20 +0300

    Final

--- Program/Program.cs
+++ Program/Program.cs
@@ -5,27 +5,35 @@
 // лучше обойтись исключительно массивами.
 
 // ["1234", "1586", "-2"] -> ["-2"]
-string[] array1 = new string[5];
-// 5 - колличество элементов массива
-int len = 3;
 
+Console.WriteLine("Введите размернотсь массива; ");
+int size = Convert.ToInt32(Console.ReadLine()!);
+string[] array1 = new string[size];
+int len = 3;// - какой размерности элемент записывать в конечный массив
 
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine($"Введите {i+1}-й элемент: ");
-    string element = Convert.ToString(Console.ReadLine()!);
-    if (element.Length <= len)
-        array1[i] = element;
-}
-
+SecondArray(array1, size);
 Console.WriteLine();
 PrintArray(array1);
 
+void SecondArray(string[] array, int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент: ");
+        string element = Convert.ToString(Console.ReadLine()!);
+        if (element.Length <= len)
+            array1[i] = element;
+    }
+
+}
+
 void PrintArray(string[] array)
 {
+    array = array.Where(x => !string.IsNullOrEmpty(x)).ToArray();
     for (int j = 0; j < array.Length; j++)
     {
-        Console.Write($"{array[j]} ");
+        Console.Write($"|{array[j]}| ");
     }
     Console.WriteLine();
 }
